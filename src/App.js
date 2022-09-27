f381f15c9b219432aa1bccf88b97babe73b63dc1
Andrew <andrew.liu.722@gmail.com>
2022-09-27 16:49:08 -0700

    finished JS tools. app.js & json files have been changed from LF to CRLF

--- src/App.js
+++ src/App.js
@@ -1,32 +1,27 @@
-const Pet = (props) => {
-    return (
-        React.createElement("div", {}, [
-            React.createElement("h1", {}, props.name),
-            React.createElement("h2", {}, props.animal),
-            React.createElement("h2", {}, props.breed),
-        ])
-    )
-}
+import React from 'react'
+import { render } from 'react-dom'
+import Pet from "./Pet"
+
 
 const App = () => {
-    return React.createElement("div", {}, [
-            React.createElement("h1", {}, "Adopt Me!"),
-            React.createElement(Pet, {
-                name: "Oni",
-                animal: "Dog",
-                breed: "Mix"
-            }),
-            React.createElement(Pet, {
-                name: "Lylie",
-                animal: "Dog",
-                breed: "Akita"
-            }),
-            React.createElement(Pet, {
-                name: "Doggie",
-                animal: "Dog",
-                breed: "Siberian-Husky"
-            }),
-        ]);
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Oni",
+      animal: "Dog",
+      breed: "Mix",
+    }),
+    React.createElement(Pet, {
+      name: "Lylie",
+      animal: "Dog",
+      breed: "Akita",
+    }),
+    React.createElement(Pet, {
+      name: "Doggie",
+      animal: "Dog",
+      breed: "Siberian-Husky",
+    }),
+  ]);
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+render(React.createElement(App), document.getElementById("root"));
