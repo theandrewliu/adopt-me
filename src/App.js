c4588aea2294b4c3775029391931707ecd11329d
Andrew <andrew.liu.722@gmail.com>
2022-10-03 13:55:08 -0700

    added a theme selector dropdown in searchparams

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import React, { StrictMode } from 'react'
+import React, { StrictMode, useState } from 'react'
 import { render } from 'react-dom'
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Pet from "./Pet"
 import SearchParams from "./SearchParams"
 import Details from './Details'
+import ThemeContext from './ThemeContext'
 
 
 const App = () => {
+    const theme = useState("darkblue");
     return (
         <StrictMode>
-            <BrowserRouter>
-                <header>
-                    <Link to="/"> Adopt Me!</Link>
-                </header>
-                <Routes>
-                    <Route path="/details/:id" element={<Details />} />
-                    <Route path="/" element={<SearchParams />} />
-                </Routes>
-            </BrowserRouter>
+            <ThemeContext.Provider value={theme}>
+                <BrowserRouter>
+                    <header>
+                        <Link to="/"> Adopt Me!</Link>
+                    </header>
+                    <Routes>
+                        <Route path="/details/:id" element={<Details />} />
+                        <Route path="/" element={<SearchParams />} />
+                    </Routes>
+                </BrowserRouter>
+            </ThemeContext.Provider>
         </StrictMode>
     );
 };
