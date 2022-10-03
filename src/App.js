89c849fd5c37f785d6d3f9cc918e07ed23237a26
Andrew <andrew.liu.722@gmail.com>
2022-10-02 18:51:47 -0700

    added react-router-dom and refactored app.js to include routers. also added a details page and changed <a> tag in pet.js to <link>

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import React from 'react'
+import React, { StrictMode } from 'react'
 import { render } from 'react-dom'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Pet from "./Pet"
 import SearchParams from "./SearchParams"
+import Details from './Details'
+
 
 const App = () => {
     return (
-        <div>
-            <h1>Adopt Me!</h1>
-            <SearchParams />
-        </div>
-    )
-}
+        <StrictMode>
+            <BrowserRouter>
+                <h1> Adopt Me!</h1>
+                <Routes>
+                    <Route path="/details/:id" element={<Details />} />
+                    <Route path="/" element={<SearchParams />} />
+                </Routes>
+            </BrowserRouter>
+        </StrictMode>
+    );
+};
 
 render(<App />, document.getElementById("root"));
--- src/Pet.js
+++ src/Pet.js
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 const Pet = ({name, animal, breed, images, location, id}) => {
 
     let hero = "http://pet-images.dev-apis.com/pets/none.jpg"
@@ -6,7 +8,7 @@ const Pet = ({name, animal, breed, images, location, id}) => {
     }
 
     return (
-        <a href={`/details/${id}`} className="pet">
+        <Link to={`/details/${id}`} className="pet">
             <div className="image-container">
                 <img src={hero} alt={name} />
             </div>
@@ -16,17 +18,9 @@ const Pet = ({name, animal, breed, images, location, id}) => {
                     {animal} - {breed} - {location}
                 </h2>
             </div>
-        </a>
+        </Link>
     );
-}
+};
 
-
-// const Pet = (props) => {
-//     return React.createElement("div", {}, [
-//       React.createElement("h1", {}, props.name),
-//       React.createElement("h2", {}, props.animal),
-//       React.createElement("h2", {}, props.breed),
-//     ]);
-//   };
 
 export default Pet;
