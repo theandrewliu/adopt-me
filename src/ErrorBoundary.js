7a0fc5a662c389a22f5284d5acee9267076ffd49
Andrew <andrew.liu.722@gmail.com>
2022-10-02 22:33:21 -0700

    fixed ErrorBoundary by adding link to. Also added a componentDidUpdate

--- src/ErrorBoundary.js
+++ src/ErrorBoundary.js
@@ -2,7 +2,7 @@ import { Component } from "react";
 import { Link} from "react-router-dom";
 
 class ErrorBoundary extends Component {
-    state = { hasError: false };
+    state = { hasError: false, redirect: false };
     static getDerivedStateFromError() {
         return { hasError: true }
     }
@@ -11,12 +11,20 @@ class ErrorBoundary extends Component {
         console.error(error, info);
     }
 
-    render () {
+    componentDidUpdate() {
         if (this.state.hasError) {
+            setTimeout(() => this.setState({ redirect: true}), 5000);
+        }
+    }
+
+    render () {
+        if (this.state.redirect){
+            return <Navigate to="/" />;
+        } else if (this.state.hasError) {
             return (
                 <h2>
                     There was an error. Oh no. What are we going to do.{" "}
-                    <Link>Click here</Link> to go back to the homepage. Or wait five
+                    <Link to="/">Click here</Link> to go back to the homepage. Or wait five
                     seconds and we will do it for you.
                 </h2>
             );
