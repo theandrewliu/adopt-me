0c91af9ab24893c143e8d4ff20b6d51a8fb41e8d
Andrew <andrew.liu.722@gmail.com>
2022-10-10 21:47:39 -0700

    added tailwind css to search params, results, and pet

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -30,8 +30,9 @@ const SearchParams = () => {
     }
 
     return (
-        <div className="search-params">
+        <div className="my-0 mx-auto w-11/12">
             <form
+                className="p-10 mb-10 rounded-lg bg-gray-200 shadow-lg flex flex-col justify-center items-center"
                 onSubmit={e => {
                     e.preventDefault();
                     requestPets();
@@ -39,11 +40,11 @@ const SearchParams = () => {
             >
                 <label htmlFor="location">
                     Location
-                    <input id="location" value={location} placeholder="Location" onChange={(e) => setLocation(e.target.value)}/>
+                    <input id="location" type="text" value={location} placeholder="Location" onChange={(e) => setLocation(e.target.value)} className="w-60 mb-5 block" />
                 </label>
                 <label htmlFor="animal">
                     Animal
-                    <select id="animal" value={animal} onChange={(e) =>{
+                    <select id="animal" className="w-60 mb-5 block" value={animal} onChange={(e) =>{
                         setAnimal(e.target.value);
                         setBreed("");
                     }}
@@ -68,6 +69,7 @@ const SearchParams = () => {
                     onBlur={(e) => {
                         setBreed(e.target.value);
                     }}
+                    className="w-60 mb-5 block disabled:opacity-50"
                     >
                     <option />
                     {breeds.map((allBreed) => (
@@ -81,6 +83,7 @@ const SearchParams = () => {
                     Theme
                     <select
                         value={theme}
+                        className="w-60 mb-5 block"
                         onChange={e => setTheme(e.target.value)}
                         onBlur={e => setTheme(e.target.value)}
                         >
@@ -91,7 +94,7 @@ const SearchParams = () => {
                             <option value="#f06d06">Fog Dog</option>
                         </select>
                 </label>
-                <button style={ {backgroundColor: theme }}>Submit</button>
+                <button className="rounded px-6 py-2 text-white hover:opacity-50 border-none" style={ {backgroundColor: theme }}>Submit</button>
             </form>
             <Results pets={pets} />
         </div>
